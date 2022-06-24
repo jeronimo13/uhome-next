13e1165f0860e0e4ad8ce66be037a0cea8a613a2
Herman Havrysh <gavrishgerman@gmail.com>
2022-06-24 09:49:56 +0300

    cart layout fixes

--- components/CartModal.tsx
+++ components/CartModal.tsx
@@ -31,17 +31,15 @@ export default function CartModal({setShowModal}) {
                 flex-col
                 w-full
                 max-w-3xl
-                h-full
                 m-auto
                 bg-white
-                rounded-lg
-                shadow-lg
+                sm:rounded-lg
+                sm:shadow-lg
+                sm:h-auto
+                h-full
                 overflow-hidden
                 "
                 >
-                    {/*content*/}
-                    {/*<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">*/}
-                    {/*header*/}
                     <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                         <h3 className="text-3xl font-semibold">Корзина</h3>
                         <button
@@ -52,13 +50,13 @@ export default function CartModal({setShowModal}) {
                         </button>
                     </div>
                     {/*body*/}
-                    <div className="p-6 overflow-y-auto">
+                    <div className="p-6 overflow-y-auto flex-1">
                         {cart.items.map((order, index) => (
                             <CartItem key={order.id} order={order} index={index} />
                         ))}
                     </div>
                     {/*footer*/}
-                    <div className="sticky bottom bg-white w-full flex items-center justify-between p-6 border-t border-solid border-slate-200 rounded-b">
+                    <div className="sticky bottom bg-white w-full flex items-center justify-between p-6 border-t border-solid border-slate-200 ">
                         <button
                             className="
                             hidden
